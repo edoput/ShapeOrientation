4f60860bbd1c535e5f968e73b21d7a7f7f468546
Edoardo Putti <edoardo.putti@gmail.com>
2018-01-16 15:28:52 +0100

    format nicely the ShapeDialog

--- ShapeOrientationDialog.py
+++ ShapeOrientationDialog.py
@@ -2,13 +2,13 @@
 """
 /***************************************************************************
 Shape Orientation
-								 A QGIS plugin
+                                                                 A QGIS plugin
  A plugin for compute the orientation of major axis of polygons and draw
  rose diagram of polygons or lines orientations
-							 -------------------
-		begin                : 2017-07
-		copyright            : 2017 F.Fouriaux - Eveha
-		email                : francois.fouriaux@eveha.fr
+                                                         -------------------
+                begin                : 2017-07
+                copyright            : 2017 F.Fouriaux - Eveha
+                email                : francois.fouriaux@eveha.fr
  ***************************************************************************/
 
 /***************************************************************************
@@ -71,14 +71,14 @@ class ShorientDialg(QDialog):
         QDialog.__init__(self)
 
     def layerPolyList(self):
-	 """
+        """
         list of layers polygons shp already charged in the interface
         """
         layers = iface.legendInterface().layers()
         layer_list = []
         self.cbox_PolyShp.clear()
         for layer in layers:
-            if layer.providerType()=='ogr':
+            if layer.providerType() == 'ogr':
                 if layer.dataProvider().storageType() == 'ESRI Shapefile' and layer.geometryType() == 2:
                     layer_list.append(layer.name())
 
@@ -98,139 +98,130 @@ class ShorientDialg(QDialog):
 
         self.cbox_LineShp.addItems(layer_list)
 
+    def selectedLayerPoly(self):
+        if self.cbox_PolyShp.currentText():
+            return selectLayer(self.cbox_PolyShp.currentText())
 
-	def selectedLayerPoly(self):
-	 
-		if self.cbox_PolyShp.currentText():
-			return selectLayer(self.cbox_PolyShp.currentText())
+    def updateIDFieldPoly(self):
+            self.cbox_PolyIdField.clear()
+            layer = self.selectedLayerPoly()
+            field_list = []
+            if layer is not None:
+                fields = layer.dataProvider().fields()
+                for f in fields:
+                    name = f.name()
+                    field_list.append(name)
+            self.cbox_PolyIdField.addItems(field_list)
 
+    def selectedLayerLine(self):
+        if self.cbox_LineShp.currentText():
+            return selectLayer(self.cbox_LineShp.currentText())
 
+    def updateIDFieldLine(self):
+        self.cbox_LineIdField.clear()
+        layer = self.selectedLayerLine()
+        field_list = []
+        if layer is not None:
+            fields = layer.dataProvider().fields()
+            for f in fields:
+                name = f.name()
+                field_list.append(name)
+        self.cbox_LineIdField.addItems(field_list)
 
-	def updateIDFieldPoly(self):
-		self.cbox_PolyIdField.clear()
-		layer = self.selectedLayerPoly()
-		field_list=[]
-		if layer is not None:            
-			fields = layer.dataProvider().fields()
-			for f in fields:
-				name = f.name()
-				field_list.append(name)
-				
-		self.cbox_PolyIdField.addItems(field_list)
+    def selectDirectory(self):
+        self.LineEdit_Output.setText(QFileDialog.getExistingDirectory(self))
 
-	def selectedLayerLine(self):
-	 
-		if self.cbox_LineShp.currentText():
-			return selectLayer(self.cbox_LineShp.currentText())
+    def OpenShp(self):
+        filtre = 'Shapefiles (*.shp)'
+        FileName = QFileDialog(filter=filtre)
+        if FileName.exec_():
+            fileName = FileName.selectedFiles()
+        AjoutShpLayer(fileName[0]) 
+        self.layerLineList()
+        self.layerPolyList()
 
+    def RunPoly(self):
+        while True:
+            poly = self.cbox_PolyShp.currentText()
+            export = self.LineEdit_Output.text()
+            interval = self.doubleSpinBox.value()
+            diagr = self.checkDiagram.isChecked()
+            colorRamp = self.cbox_ColorRamp.currentText()
+            Id = self.cbox_PolyIdField.currentText()
+            if poly != '':
+                pass
+            else :
+                iface.messageBar().pushMessage("Error", "No Layer Selected", level=QgsMessageBar.WARNING)
+                break
+            if export=='':
+                iface.messageBar().pushMessage("Error", "No export folder selected", level=QgsMessageBar.WARNING)
+                break
+            elif os.path.exists(export):
+                pass 
+            else: 
+                iface.messageBar().pushMessage("Error", "The export folder selected doesn't exists", level=QgsMessageBar.WARNING)
+                break
+            shpPath = ''
+            for layer in iface.legendInterface().layers():
+                if layer.name() == poly:
+                    a = layer.publicSource()
+                    if a[0:5] == 'file:':
+                        b = str(a.split('?')[0])
+                        shpPath = urllib.unquote(b)[7:]
+                        if os.name == 'nt':
+                            shpPath = shpPath[1:]
+                    else:
+                        shpPath = a
+                        if os.name == 'nt':
+                            shpPath = shpPath[1:]
+            table = export+'/Orientation_'+poly+'.csv'
+            DiagOrientPolyg (shpPath, interval, table, diagr, colorRamp, Id)
+            AjoutCsvLayer(table)
+            iface.messageBar().clearWidgets()
+            iface.messageBar().pushMessage(
+                "ShapeOrientation",
+                "Orientation Completed",
+                level=QgsMessageBar.SUCCESS,
+            )
+            break
 
-
-	def updateIDFieldLine(self):
-		self.cbox_LineIdField.clear()
-		layer = self.selectedLayerLine()
-		field_list=[]
-		if layer is not None:            
-			fields = layer.dataProvider().fields()
-			for f in fields:
-				name = f.name()
-				field_list.append(name)
-				
-		self.cbox_LineIdField.addItems(field_list)
-
-   
-	def selectDirectory(self):
-		self.LineEdit_Output.setText(QFileDialog.getExistingDirectory(self))
-
-	def OpenShp(self):
-		filtre= 'Shapefiles (*.shp)'
-		FileName= QFileDialog(filter=filtre)
-		if FileName.exec_():
-			fileName=FileName.selectedFiles()
-		AjoutShpLayer(fileName[0]) 
-		self.layerLineList()
-		self.layerPolyList()
-		
-		
-	def RunPoly (self): 
-		while True:
-			poly=self.cbox_PolyShp.currentText()
-			export=self.LineEdit_Output.text()
-			interval=self.doubleSpinBox.value()
-			diagr=self.checkDiagram.isChecked()
-			colorRamp=self.cbox_ColorRamp.currentText()
-			Id=self.cbox_PolyIdField.currentText()
-			if poly != '':
-				pass
-			else :
-				iface.messageBar().pushMessage("Error", "No Layer Selected", level=QgsMessageBar.WARNING)
-				break
-			if export=='':
-				iface.messageBar().pushMessage("Error", "No export folder selected", level=QgsMessageBar.WARNING)
-				break
-			elif os.path.exists(export):
-				pass 
-			else: 
-				iface.messageBar().pushMessage("Error", "The export folder selected doesn't exists", level=QgsMessageBar.WARNING)
-				break
-			shpPath=''
-			for layer in iface.legendInterface().layers():
-				if layer.name() == poly:
-					a= layer.publicSource()
-					if a[0:5] =='file:':
-						b=str(a.split('?')[0])
-						shpPath=urllib.unquote(b)[7:]
-						if os.name=='nt':
-							shpPath=shpPath[1:]
-					else:
-						shpPath=a
-						if os.name=='nt':
-							shpPath=shpPath[1:]
-			table=export+'/Orientation_'+poly+'.csv'
-			DiagOrientPolyg (shpPath, interval, table, diagr,colorRamp,Id)
-			AjoutCsvLayer(table)
-			iface.messageBar().clearWidgets()
-			iface.messageBar().pushMessage("ShapeOrientation", "Orientation Completed", level=QgsMessageBar.SUCCESS)
-			break 
-			
-	def RunLine (self): 
-		while True:
-			line=self.cbox_LineShp.currentText()
-			export=self.LineEdit_Output.text()
-			interval=self.doubleSpinBox_2.value()
-			diagr=self.checkDiagram.isChecked()
-			colorRamp=self.cbox_ColorRamp.currentText()
-			Id=self.cbox_LineIdField.currentText()
-			if line != '':
-				pass
-			else :
-				iface.messageBar().pushMessage("Error", "No Layer Selected", level=QgsMessageBar.WARNING)
-				break
-			if export=='':
-				iface.messageBar().pushMessage("Error", "No export folder selected", level=QgsMessageBar.WARNING)
-				break
-			elif os.path.exists(export):
-				pass 
-			else: 
-				iface.messageBar().pushMessage("Error", "The export folder selected doesn't exists", level=QgsMessageBar.WARNING)
-				break
-			shpPath=''
-			for layer in iface.legendInterface().layers():
-				if layer.name() == line:
-					a= layer.publicSource()
-					if a[0:5] =='file:':
-						b=str(a.split('?')[0])
-						shpPath=urllib.unquote(b)[7:]
-						if os.name=='nt':
-							shpPath=shpPath[1:]
-					else:
-						shpPath=a
-						if os.name=='nt':
-							shpPath=shpPath[1:]
-			table=export+'/Orientation_'+line+'.csv'
-			DiagOrientLine (shpPath, interval, table, diagr,colorRamp,Id)
-			AjoutCsvLayer(table)
-			iface.messageBar().clearWidgets()
-			iface.messageBar().pushMessage("ShapeOrientation", "Orientation Completed", level=QgsMessageBar.SUCCESS)
-			break 
-
-   
+    def RunLine(self):
+        while True:
+            line = self.cbox_LineShp.currentText()
+            export = self.LineEdit_Output.text()
+            interval = self.doubleSpinBox_2.value()
+            diagr = self.checkDiagram.isChecked()
+            colorRamp = self.cbox_ColorRamp.currentText()
+            Id = self.cbox_LineIdField.currentText()
+            if line != '':
+                pass
+            else :
+                iface.messageBar().pushMessage("Error", "No Layer Selected", level=QgsMessageBar.WARNING)
+                break
+            if export == '':
+                iface.messageBar().pushMessage("Error", "No export folder selected", level=QgsMessageBar.WARNING)
+                break
+            elif os.path.exists(export):
+                pass
+            else:
+                iface.messageBar().pushMessage("Error", "The export folder selected doesn't exists", level=QgsMessageBar.WARNING)
+                break
+            shpPath = ''
+            for layer in iface.legendInterface().layers():
+                if layer.name() == line:
+                    a = layer.publicSource()
+                    if a[0:5] =='file:':
+                        b = str(a.split('?')[0])
+                        shpPath = urllib.unquote(b)[7:]
+                        if os.name=='nt':
+                            shpPath=shpPath[1:]
+                    else:
+                        shpPath=a
+                        if os.name=='nt':
+                            shpPath=shpPath[1:]
+            table = export+'/Orientation_'+line+'.csv'
+            DiagOrientLine (shpPath, interval, table, diagr, colorRamp, Id)
+            AjoutCsvLayer(table)
+            iface.messageBar().clearWidgets()
+            iface.messageBar().pushMessage("ShapeOrientation", "Orientation Completed", level=QgsMessageBar.SUCCESS)
+            break
--- ShapeOrientation_dialog.py
+++ ShapeOrientation_dialog.py
@@ -2,13 +2,13 @@
 """
 /***************************************************************************
  ShapeOrientationDialog
-								 A QGIS plugin
+                                                                 A QGIS plugin
  This plugin calculate a diagrame of the major orientations of polygons 
-							 -------------------
-		begin                : 2017-07-24
-		git sha              : $Format:%H$
-		copyright            : (C) 2017 by FFouriaux Eveha
-		email                : francois.fouriaux@eveha.fr
+                                                         -------------------
+                begin                : 2017-07-24
+                git sha              : $Format:%H$
+                copyright            : (C) 2017 by FFouriaux Eveha
+                email                : francois.fouriaux@eveha.fr
  ***************************************************************************/
 
 /***************************************************************************
@@ -29,33 +29,33 @@ from PyQt4.QtGui import *
 from ShapeOrientationDialog import *
 from ShapeOrientation_engine import *
 
-image=os.path.join(os.path.dirname(__file__),"images")
+image = os.path.join(os.path.dirname(__file__), "images")
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
-	os.path.dirname(__file__), 'ShapeOrientation_dialog_base.ui'))
+        os.path.dirname(__file__), 'ShapeOrientation_dialog_base.ui'))
 
 
 class ShapeOrientationDialog(ShorientDialg, FORM_CLASS):
-	def __init__(self, parent=None):
-		"""Constructor."""
-		ShorientDialg.__init__(self)
-		# Set up the user interface from Designer.
-		# After setupUI you can access any designer object by doing
-		# self.<objectname>, and you can use autoconnect slots - see
-		# http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-		# #widgets-and-dialogs-with-auto-connect
-		self.setupUi(self)
-		self.layerPolyList()
-		self.layerLineList()
-		self.cbox_ColorRamp.addItem(QIcon(os.path.join(image,"AutomnRamp.png")),"autumn")
-		self.cbox_ColorRamp.addItem(QIcon(os.path.join(image,"WinterRamp.png")),"winter")
-		self.cbox_ColorRamp.addItem(QIcon(os.path.join(image,"SummerRamp.png")),"summer")
-		self.cbox_ColorRamp.addItem(QIcon(os.path.join(image,"BoneRamp.png")),"bone")
-		self.cbox_ColorRamp.addItem(QIcon(os.path.join(image,"GreyRamp.png")),"gray")
-		QObject.connect(self.pushButton_Output, SIGNAL('clicked()'),self.selectDirectory)
-		QObject.connect(self.pushButton_1, SIGNAL('clicked()'),self.OpenShp)
-		QObject.connect(self.pushButton_2, SIGNAL('clicked()'),self.OpenShp)
-		QObject.connect(self.cbox_PolyShp, SIGNAL('currentIndexChanged(QString)'), self.updateIDFieldPoly)
-		QObject.connect(self.cbox_LineShp, SIGNAL('currentIndexChanged(QString)'), self.updateIDFieldLine)
-		QObject.connect(self.pushButton_Poly, SIGNAL('clicked()'),self.RunPoly)
-		QObject.connect(self.pushButton_Line, SIGNAL('clicked()'),self.RunLine)
+    def __init__(self, parent=None):
+        """Constructor."""
+        ShorientDialg.__init__(self)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
+        self.layerPolyList()
+        self.layerLineList()
+        self.cbox_ColorRamp.addItem(QIcon(os.path.join(image,"AutomnRamp.png")),"autumn")
+        self.cbox_ColorRamp.addItem(QIcon(os.path.join(image,"WinterRamp.png")),"winter")
+        self.cbox_ColorRamp.addItem(QIcon(os.path.join(image,"SummerRamp.png")),"summer")
+        self.cbox_ColorRamp.addItem(QIcon(os.path.join(image,"BoneRamp.png")),"bone")
+        self.cbox_ColorRamp.addItem(QIcon(os.path.join(image,"GreyRamp.png")),"gray")
+        QObject.connect(self.pushButton_Output, SIGNAL('clicked()'),self.selectDirectory)
+        QObject.connect(self.pushButton_1, SIGNAL('clicked()'),self.OpenShp)
+        QObject.connect(self.pushButton_2, SIGNAL('clicked()'),self.OpenShp)
+        QObject.connect(self.cbox_PolyShp, SIGNAL('currentIndexChanged(QString)'), self.updateIDFieldPoly)
+        QObject.connect(self.cbox_LineShp, SIGNAL('currentIndexChanged(QString)'), self.updateIDFieldLine)
+        QObject.connect(self.pushButton_Poly, SIGNAL('clicked()'),self.RunPoly)
+        QObject.connect(self.pushButton_Line, SIGNAL('clicked()'),self.RunLine)
